73c429601909f08de86cc8a6a0c411adb0f0386d
Aaditya Sharma <114431922+aadityasharmax@users.noreply.github.com>
2025-03-21 10:03:45 +0530

    Add files via upload

---
+++ src/components/Input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { BiPlusCircle } from "react-icons/bi";
+
+const Input = ({ onOpen, filterContacts }) => {
+  return (
+    <div className=" flex  relative items-center gap-2 ">
+      <FaSearch className="text-white text-2xl absolute ml-1" />
+      <input
+        onChange={filterContacts}
+        type="text"
+        className="flex flex-grow text-white outline-0 border pr-2 pl-10 text-lg border-white h-10 bg-transparent rounded-lg "
+      />
+      <BiPlusCircle
+        className="text-white text-4xl cursor-pointer"
+        onClick={() => onOpen()}
+      />
+    </div>
+  );
+};
+
+export default Input;
